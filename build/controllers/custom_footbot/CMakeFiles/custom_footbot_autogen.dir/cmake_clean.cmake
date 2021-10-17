c451c9162f91993f8d460f8b718df64d4a54a303
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2021-10-17 15:12:10 -0600

    Delete build directory

--- build/controllers/custom_footbot/CMakeFiles/custom_footbot_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/custom_footbot_autogen"
-  "custom_footbot_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/custom_footbot_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
