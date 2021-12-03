ec83ea4513892a1cf2c5b22d01c4cfa80324f66b
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2021-12-02 20:39:04 -0700

    Update Driveto.lua

--- lua-only/Driveto.lua
+++ lua-only/Driveto.lua
@@ -12,4 +12,14 @@ function driveTo(x,y)
 	
 	--convert quarternion to z rotation
 	current_angle=math.deg(math.asin(robot.positioning.orientation.z)*2)
+	
+	--create unit vector for the face direction
+	
+	--Cross product of trajectory vector with face direction vector
+	--T x F
+	--If the result is negative, Turn Left. Otherwise turn Right.
+	
+	--Angle between them is inverse cosine of the dot product T and F divided by the manitude of Trajectory vector.
+	--acos(F*T/|T|)*180/pi
+	--This angle will always be between 0-180
 end
