915c662b9df350487e46cd0b3b55700d229c421d
GummyBear2650 <96498769+GummyBear2650@users.noreply.github.com>
2021-12-26 13:59:01 -0700

    Create Extractor1.py
    Converting the Arena Size command in ARGoS to GPR Max syntax

---
+++ KissingBooth/Extractor1.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('SomeFile.xml')
+root = tree.getroot()
+
+def getArenaSize(root):
+    for child in root:
+        if child.tag == 'arena':
+            return(child.attrib['size'])
+
+size=getArenaSize(root)
+gprDomain='#domain ' + size.replace(',','')
+
+print(gprDomain)
