0aa5331aee28e1e08635302b4f17a4b1d14d9fc5
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-01-17 16:21:16 -0700

    Moved files. Created GPR-Movement.lua based on the prototype implementation

---
+++ lua-only/movement-MVP/GPR-Movement.lua
@@ -0,0 +1,20 @@
+function init()
+    reset()
+end
+
+function step()
+    log("wheelvelocities:"..
+    string.format("%.2f", robot.joints.base_wheel_bl.encoder) ..","..
+    string.format("%.2f", robot.joints.base_wheel_fl.encoder) ..","..
+    string.format("%.2f", robot.joints.base_wheel_br.encoder) ..","..
+    string.format("%.2f", robot.joints.base_wheel_fr.encoder))
+end
+
+function reset()
+    robot.joints.base_wheel_bl.set_target(1)
+    robot.joints.base_wheel_fl.set_target(1)
+    robot.joints.base_wheel_br.set_target(1)
+    robot.joints.base_wheel_fr.set_target(1)
+end
+
+function destroy() end
