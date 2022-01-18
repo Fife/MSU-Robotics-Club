fde539f51145d76227e572ce486f4e47b4b7c4e7
GummyBear2650 <96498769+GummyBear2650@users.noreply.github.com>
2022-01-18 11:28:14 -0700

    Update Extractor1.py
    added title extraction

--- booth/Extractor1.py
+++ booth/Extractor1.py
@@ -1,16 +1,27 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('SomeFile.xml')
+tree = ET.parse('GPR-antenna.xml')
 root = tree.getroot()
+
+def getTitle(root):
+    for child in root:
+        if child.tag == 'gprMax':
+            return(child.attrib['title'])
 
 def getArenaSize(root):
     for child in root:
         if child.tag == 'arena':
             return(child.attrib['size'])
+               
+title=getTitle(root)
+gprTitle='#title: ' + title
 
 size=getArenaSize(root)
 gprDomain='#domain ' + size.replace(',','')
 
 success = open("gprDomain.txt", "w")
+success.write(gprTitle)
+success.write("\n")
 success.write(gprDomain)
 success.close()
+print(gprTitle)
