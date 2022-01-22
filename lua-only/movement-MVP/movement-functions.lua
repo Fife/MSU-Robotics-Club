fd0b204ed4c683a2f7b4ee1a70c2dfaedf26505c
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-01-22 14:26:14 -0700

    Replaced the drive to function with the newer one

--- lua-only/movement-MVP/movement-functions.lua
+++ lua-only/movement-MVP/movement-functions.lua
@@ -26,46 +26,55 @@ function reverse(velocity)
     robot.joints.base_wheel_fr.set_target(velocity)
 end
 
-function driveTo(x,y, forward_velocity)
+function driveTo(x,y)
 	--get x and y of robot
-	current_x= robot.positioning.position.x
-	current_y= robot.positioning.position.y
-	
+	local current_x= robot.positioning.position.x
+	local current_y= robot.positioning.position.y
 	--create cartesian trajectory vector table
-	traj_vector_cart = {x - current_x, y - current_y}
+
+
+	local traj_vector_cart = {x - current_x, y - current_y}
 	
 	--calculate magnitude of trajectory
-	sum_of_squares=math.pow(traj_vector_cart[1],2)+math.pow(traj_vector_cart[2],2)
-	traj_magnitude=math.sqrt((sum_of_sqaures))	
+	local sum_of_squares=math.pow((x - current_x),2)+math.pow((y - current_y),2)
+
+	local traj_magnitude=math.sqrt(sum_of_squares)	
 
 	--convert quarternion to z rotation
-	current_angle=math.asin(robot.positioning.orientation.z)*2
-	
+	local current_angle=math.asin(robot.positioning.orientation.z)*2
+
 	--create cartesian face vector 
-	face_vector_cart = {math.cos(current_angle), math.sin(current_angle)}
+
+	local face_vector_cart = {math.cos(current_angle), math.sin(current_angle)}
 	
 	--Cross product of trajectory vector with face direction vector
 	--Shortcut for 2d vectors since theres no cross product function.
 	--T x F <-- Order is important
-	cross_p = (traj_vector_cart[1]*face_vector_cart[2])-(face_vector_cart[1]*traj_vector_cart[2])
+	local cross_p = (traj_vector_cart[1]*face_vector_cart[2])-(face_vector_cart[1]*traj_vector_cart[2])
 	
 	--Angle between them is inverse cosine of the dot product T and F divided by the manitude of Trajectory vector.
 	--acos(F*T/|T|)
 	--This angle will always be between 0-180
-	dot_p = face_vector_cart[1]*traj_vector_cart[1] + face_vector_cart[2]*traj_vector_cart[2]
-	a = math.acos(dot_p/traj_magnitude)
+	local dot_p = face_vector_cart[1]*traj_vector_cart[1] + face_vector_cart[2]*traj_vector_cart[2]
+	local a = math.deg((math.acos(dot_p/traj_magnitude)))
 	
 	
+
+	log("Angle between :" .. a)
+	log("Cross Product:" .. cross_p)
+	log("Face Angle " .. math.deg(current_angle))
 	--If the result is negative, Turn Left. Otherwise turn Right. We can use the angle to determine how much we need to turn.
-	speed_ratio = 7 *(a/180)
-	if cross_p < 0 then
-		turnLeft(speed_ratio)
+	local speed_ratio = 10 
 	
-	elseif cross_p > 0 then 
-		turnRight(speed_ratio)
+	if cross_p < -0.005 then
+		turnLeft(speed_ratio/2)
 	
-	else
-		driveForward(forward_velocity)
+	elseif cross_p > 0.005 then 
+		turnRight(speed_ratio/2)
 	
+	elseif cross_p < 0.005 and cross_p > -0.005 then
+		if a < 2 then
+				robot.wheels.set_velocity(speed_ratio*2, speed_ratio*2)			
+		end
 	end
 end
