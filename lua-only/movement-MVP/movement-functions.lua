b5d3861fce90363cc55af7ac7792a832fe550dfd
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-01-22 11:04:23 -0700

    Create movement-functions.lua

---
+++ lua-only/movement-MVP/movement-functions.lua
@@ -0,0 +1,20 @@
+function driveForward()
+    robot.joints.base_wheel_bl.set_target(-1)
+    robot.joints.base_wheel_fl.set_target(-1)
+    robot.joints.base_wheel_br.set_target(1)
+    robot.joints.base_wheel_fr.set_target(1)
+end
+
+function turnLeft()
+    robot.joints.base_wheel_bl.set_target(0)
+    robot.joints.base_wheel_fl.set_target(0)
+    robot.joints.base_wheel_br.set_target(1)
+    robot.joints.base_wheel_fr.set_target(1)
+end
+
+function turnRight()
+    robot.joints.base_wheel_bl.set_target(-1)
+    robot.joints.base_wheel_fl.set_target(-1)
+    robot.joints.base_wheel_br.set_target(0)
+    robot.joints.base_wheel_fr.set_target(0)
+end
