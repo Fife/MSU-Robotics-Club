2ece0751872f684de4ae8bef254e8abcf15a5bb3
MitchellThompson1 <98499892+MitchellThompson1@users.noreply.github.com>
2022-01-29 17:48:41 -0700

    Update getSensorPos.lua

--- booth/getSensorPos.lua
+++ booth/getSensorPos.lua
@@ -1,3 +1,6 @@
+-- Takes rover's x,y coordinates, the offset of the sensors from
+-- the center of the rover d, and rover's direction in radians dir.
+-- Returns the coordinates of the transmitter and the receiver.
 function getSensorPos(x, y, d, dir)
     local transX = (x + d*math.cos(dir))
     local transY = (y + d*math.sin(dir))
