eb49ebc614417695037944faef72dfbefa0a7735
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-01-31 14:32:50 -0700

    Update booth.py
    Added try except structure to help debugging plugin

--- booth/booth.py
+++ booth/booth.py
@@ -5,7 +5,24 @@ from Extractor1 import *
 
 experiment_path = sys.argv[1]
 
-root = getRoot(experiment_path)
-
-print(getTitle(root))
+try:
+  experiment_path = sys.argv[1]
+  
+except IndexError:
+  print("Nothing was passed in as an argument")
+  
+else:
+  root = getRoot(experiment_path)
+  
+  try:
+    generateIN(root)
+  except:
+    print(experiment_path, "is an invalid experiment path")
+  else:
+    # The root is the XML tree of the experiment.
+    # If the xml has succussfully been parsed:
+    #   Generate the .in file for gprMax
+    print(getTitle(root))
+    
+  
 
