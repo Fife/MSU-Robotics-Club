df11f0bcc1153ae0fe4be328b674d94d4a3d69d3
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-01-31 14:45:13 -0700

    Update booth.py
    removed elements from outside the try except blocks. Fixed both error catches so they print correctly now.

--- booth/booth.py
+++ booth/booth.py
@@ -1,20 +1,17 @@
 import getopt, sys
-from Extractor1 import * 
+from quickxmltest import * 
 
 #Only argument is the path of the argos experiment file
-
-experiment_path = sys.argv[1]
 
 try:
   experiment_path = sys.argv[1]
   
 except IndexError:
-  print("Nothing was passed in as an argument")
+  print("Erorr, Nothing was passed in as an argument.")
   
 else:
-  root = getRoot(experiment_path)
   try:
-    generateIN(root)
+    root = getRoot(experiment_path)
     
   except:
     print(experiment_path, "is an invalid experiment path")
@@ -24,6 +21,5 @@ else:
     # If the xml has succussfully been parsed:
     #   Generate the .in file for gprMax
     print(getTitle(root))
-    
   
 
