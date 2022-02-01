25489590c64959d46a47f263fd47854df9ae3d3b
MitchellThompson1 <98499892+MitchellThompson1@users.noreply.github.com>
2022-01-31 17:53:32 -0700

    Update getSensorPos.lua

--- booth/getSensorPos.lua
+++ booth/getSensorPos.lua
@@ -1,8 +1,11 @@
--- Takes rover's coordinates 'x,y' the distance of the sensors from
--- the center of the rover 'd' and rover's direction in degrees 'dir'
--- Returns the coordinates of the transmitter and the receiver.
+--[[
+Takes rover's coordinates 'x,y,z' the distance of the sensors from
+the center of the rover 'd' and rover's direction in degrees 'dir'
+Returns the address of a table with the coordinates of the transmitter
+and the receiver.
+]]--
 function getSensorPos(x, y, z, d, dir)
-    -- Convert orientation from degrees to radians
+    -- Convert direction from degrees to radians
     dir = dir*0.01745329252;
     
     -- Transmitter coordinates
@@ -15,5 +18,5 @@ function getSensorPos(x, y, z, d, dir)
     local recY = (y - d*math.sin(dir))
     local recZ = z
     
-    return {transX, transY, recX, recY}
+    return {transX, transY, transZ, recX, recY, recZ}
 end
