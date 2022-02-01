375dc798f5edcfd2c2b7246ea4b4b6bf77b4d535
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-02-01 14:36:21 -0700

    Update booth.py

--- booth/booth.py
+++ booth/booth.py
@@ -10,9 +10,9 @@ except IndexError:
   print("Erorr, Nothing was passed in as an argument.")
   
 else:
+  experiment_path = sys.argv[1]
   try:
     root = getRoot(experiment_path)
-    
   except:
     print(experiment_path, "is an invalid experiment path")
     
@@ -20,6 +20,7 @@ else:
     # The root is the XML tree of the experiment.
     # If the xml has succussfully been parsed:
     #   Generate the .in file for gprMax
+    root = getRoot(experiment_path)
     print(getTitle(root))
   
 
