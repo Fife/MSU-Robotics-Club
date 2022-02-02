5faa0f9674767bbb7c5d5bbb54901e1047d292ef
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-02-01 19:59:19 -0700

    Update gprMaxInterface.lua
    Started working on gpr_step function

--- lua-only/GPR-MVP/gprMaxInterface.lua
+++ lua-only/GPR-MVP/gprMaxInterface.lua
@@ -1,20 +1,26 @@
 local sh = require ('sh')
 
 local python_extractor = 'booth/Extractor1.py'
+local gpr_updater = 'booth/gprUpdater.py'
 
 local python = sh.command('python')
 local launch_gprmax = sh.command('python', '-m', 'gprMax')
 
 function gpr_init(argos_exp_path)
   --Copy file over to the booth
-  cp(argos_exp_path, '/booth')
+  cp(argos_exp_path, '/booth/current-sim')
   --Launch python script to generate the gprMax ".in" file
   python(python_extractor, argos_exp_path)
  
 end
 
-function gpr_step(Tx_coordinate, Rx_coordinate)
+function gpr_step(Tx, Rx)
+  --Convert coordinates to form "1 1 1" for the python conversion
+  transmitter_string = tostring(Tx[1]) .. " " .. tostring(Tx[2]) .. " " .. tostring(Tx[3])
+  receiver_string = tostring(Rx[1]) .. " " .. tostring(Rx[2]) .. " " .. tostring(Rx[3])
+  
   --Pass the calculated coordinates into the gprMax model
+  python(gpr_updater, path, transmitter_string, receiver_string)
   --Create a new directory to put the results into
   --Run the simulation for that Tx and Rx position, write results in created directory
 end
