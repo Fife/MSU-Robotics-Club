636978a5ab3dd6dc08389d0d58e59a0e4c7cfb76
CamiJolly1 <99227356+CamiJolly1@users.noreply.github.com>
2022-02-28 19:18:13 -0700

    Create Chunk.lua

---
+++ lua-only/Chunk.lua
@@ -0,0 +1,17 @@
+function chunk(xLength, yLength, numSqr)
+ squareList = {}
+      for j = 0,numSqr,1
+        do
+        for i = 0,numSqr,1
+            do
+            tl={x=(xLength/numSqr)*i, y=(yLength/numSqr)*(j+1)}
+            bl={x=(xLength/numSqr)*i, y=(yLength/numSqr)*j}
+            tr={x=(xLength/numSqr)*(i+1), y=(yLength/numSqr)*(j+1)}
+            br={x=(xLength/numSqr)*(i+1), y=(yLength/numSqr)*j}
+            square= {tl, bl, tr, br}
+            table.insert(squareList, square)
+        end
+    end
+    return squareList
+end
+print(chunk(50,50,5))
