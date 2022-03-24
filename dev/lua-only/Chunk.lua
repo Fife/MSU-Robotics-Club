d03d4d5219710abc04b145903e4736442d3cf958
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-03-23 21:56:15 -0600

    update from earlier

--- dev/lua-only/Chunk.lua
+++ dev/lua-only/Chunk.lua
@@ -1,17 +1,18 @@
-function chunk(xLength, yLength, numSqr)
- squareList = {}
-      for j = 0,numSqr,1
-        do
-        for i = 0,numSqr,1
-            do
-            tl={x=(xLength/numSqr)*i, y=(yLength/numSqr)*(j+1)}
-            bl={x=(xLength/numSqr)*i, y=(yLength/numSqr)*j}
-            tr={x=(xLength/numSqr)*(i+1), y=(yLength/numSqr)*(j+1)}
-            br={x=(xLength/numSqr)*(i+1), y=(yLength/numSqr)*j}
-            square= {tl, bl, tr, br}
-            table.insert(squareList, square)
-        end
-    end
-    return squareList
+function chunk(xLength, yLength, numSqr, center)
+    squareList = {}
+    offset =  {x=center[1]-(0.5*xLength) , y=center[2] -(0.5*yLength)}
+     for j = 0,numSqr,1
+       do
+       for i = 0,numSqr,1
+           do
+           tl={x=(xLength/numSqr)*i+offset["x"], y=(yLength/numSqr)*(j+1)+offset["y"]}
+           bl={x=(xLength/numSqr)*i+offset["x"], y=(yLength/numSqr)*j+offset["y"]}
+           tr={x=(xLength/numSqr)*(i+1)+offset["x"], y=(yLength/numSqr)*(j+1)+offset["y"]}
+           br={x=(xLength/numSqr)*(i+1)+offset["x"], y=(yLength/numSqr)*j+offset["y"]}
+           square= {tl = tl, bl = bl, tr=tr, br=br}
+           table.insert(squareList, square)
+       end
+   end
+   return squareList
 end
 print(chunk(50,50,5))
